1b0d25f7b38ff0eb64a0556c2cca450b7a06d510
Davi Chaves <dchaves@usc.edu>
2016-06-13 16:00:04 -0700

    mainmenu ready

---
+++ PeevedPenguins/MainMenu.swift
@@ -0,0 +1,46 @@
+//
+//  MainMenu.swift
+//  PeevedPenguins
+//
+//  Created by Davi Rodrigues Chaves on 6/13/16.
+//  Copyright © 2016 davichaves. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MainMenu: SKScene {
+    
+    /* UI Connections */
+    var buttonPlay: MSButtonNode!
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        
+        /* Set UI connections */
+        buttonPlay = self.childNodeWithName("buttonPlay") as! MSButtonNode
+        
+        /* Setup restart button selection handler */
+        buttonPlay.selectedHandler = {
+            
+            /* Grab reference to our SpriteKit view */
+            let skView = self.view as SKView!
+            
+            /* Load Game scene */
+            let scene = GameScene(fileNamed:"GameScene") as GameScene!
+            
+            /* Ensure correct aspect mode */
+            scene.scaleMode = .AspectFill
+            
+            /* Show debug */
+            skView.showsPhysics = true
+            skView.showsDrawCount = true
+            skView.showsFPS = true
+            
+            /* Start game scene */
+            skView.presentScene(scene)
+        }
+        
+    }
+    
+}
